e7f47421ecdff286fcd377b8723c9c16835a4153
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-04-26 14:21:01 -0400

    update dogecoin

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ fetch(
   .then((data) => {
     console.log(data);
     document.body.style.backgroundImage = `url('${data.urls.full}')`;
-    document.getElementById('author').textContent = ` By: ${data.user.name}`
+    document.getElementById("author").textContent = ` By: ${data.user.name}`;
   })
-  .catch(err => {
-    console.log("someghing went wrong")
+  .catch((err) => {
+    console.log("someghing went wrong");
     document.body.style.backgroundImage = "url('error.jpg')";
+  });
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
+
+    document.getElementById('test').innerHTML = `
+    <img src=${data.image.small}/>
+    <spam>${data.market_data.ath.usd} usd</spam>
+    `
   })
-
-
+  .catch((err) => console.log("ups, there is problem"));
